67a0bc2a2103a546c1883bfa10ed6827f60c8396
Евгений Меркулов <i_zhenya.z@mail.ru>
2018-04-01 23:04:04 +0300

    Initital

---
+++ ObjectOverrides/Person.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ObjectOverrides
+{
+    class Person
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public int Age { get; set; }
+        public Person (string firstName, string familyName, int age)
+        {
+            FirstName = firstName;
+            SecondName = familyName;
+            Age = age;
+        }
+        public Person()
+        { }
+        public override string ToString()
+        {
+            string str = string.Format("First Name: {0}, Last Name: {1}, Age: {2}", FirstName, SecondName, Age);
+            return str;
+        }
+        public override bool Equals(object obj)
+        {
+            if (this.ToString()==obj.ToString())
+                return true;
+            else
+                return false;
+        }
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+    }
+}
---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Fun with System.Object");
+            Person p1 = new Person("Homer", "Simpson", 50);
+            Person p4 = new Person("Homer", "Simpson", 50);
+            Console.WriteLine("ToString: {0}", p1.ToString());
+            Console.WriteLine("GetHash: {0}", p1.GetHashCode());
+            Console.WriteLine("GetType: {0}", p1.GetType());
+
+            Person p2 = p1;
+            object o = p2;
+            if (o.Equals(p1) && p2.Equals(o))
+                Console.WriteLine("Same reference");
+            Console.WriteLine(object.Equals(p1, p4));
+            Console.WriteLine(object.ReferenceEquals(p1,p4));
+            Console.ReadKey();
+        }
+    }
+}
